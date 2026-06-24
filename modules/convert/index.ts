6d7e747beeaf5d2c893cc46a2fcb97c008b8e1bf
mrchn <mrnfll@icloud.com>
2026-06-24 23:54:20 +0300

    fixed convertion module in Expo Go

--- modules/convert/index.ts
+++ modules/convert/index.ts
@@ -1,17 +1,16 @@
 // @/modules/convert (pdfcraft-mobile)
 
-import { requireOptionalModule } from 'expo';
-
-const Convert = requireOptionalModule('Convert');
+import { NativeModules } from 'react-native';
 
 export async function localConvert (
 	docxPath: string, outputPath: string
 ): Promise<boolean> {
 
+	const ConvertModule = NativeModules.Convert || NativeModules.ExpoConvert;
 	if (!Convert) return false;
 	try {
 		const cleanDocx = docxPath.replace('file://', '');
 		const cleanOutput = outputPath.replace('file://', '');
-		return await Convert.convert(cleanDocx, cleanOutput);
+		return await ConvertModule.convert(cleanDocx, cleanOutput);
 	} catch { return false }
 }

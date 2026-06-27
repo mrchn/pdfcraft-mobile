b11d2bd465650baea75c56bdb3788e73ae7678b1
mrchn <mrnfll@icloud.com>
2026-06-27 03:06:28 +0300

    added expo-modules-core import

--- modules/ios-native-convert/src/IosNativeConvertModule.ts
+++ modules/ios-native-convert/src/IosNativeConvertModule.ts
@@ -1,6 +1,6 @@
 // @/modules/ios-native-convert/src/IosNativeConvertModule.ts (pdfcraft-mobile)
 
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from 'expo-modules-core';
 
 declare class IosNativeConvertModule extends NativeModule {
 	convert(docxPath: string, outputPath: string): Promise<boolean>

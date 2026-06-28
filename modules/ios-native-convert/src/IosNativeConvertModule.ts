63ae70acdefa05f23de1b4cf2e9455e1ec8600ee
mrchn <mrnfll@icloud.com>
2026-06-28 20:08:01 +0300

    changed expo-modules-core to expo

--- modules/ios-native-convert/src/IosNativeConvertModule.ts
+++ modules/ios-native-convert/src/IosNativeConvertModule.ts
@@ -1,6 +1,6 @@
 // @/modules/ios-native-convert/src/IosNativeConvertModule.ts
 
-import { requireNativeModule } from 'expo-modules-core';
+import { requireNativeModule } from 'expo';
 
 interface IosNativeConvertModule {
 	convert(docxPath: string, outputPath: string): Promise<boolean>

de3204c10f4d00d6aedd005608cad9a63b0315a4
mrchn <mrnfll@icloud.com>
2026-06-30 02:18:50 +0300

    removed error alert

--- services/create.ts
+++ services/create.ts
@@ -1,6 +1,5 @@
 // @/services/create
 
-import { Alert } from 'react-native'
 import * as FileSystem from 'expo-file-system/legacy'
 import * as Sharing from 'expo-sharing'
 import { Generate } from './server'
@@ -9,26 +8,21 @@ import { convertPDF } from './convert'
 type Params = {
 	doc: { uri: string ; title: string }
 	data: Record<string, string>
-	t: (key: string) => string
 }
 
-export async function Create ({ doc, data, t }: Params) {
-
+export async function Create({ doc, data }: Params) {
 	const uri = await Generate(doc.uri, doc.title, data)
-	if (uri) {
-		const docx_title = uri.split('/').pop()
-			|| `crafted_${doc.title}`
-		const pdf_title = docx_title.replace('.docx', '.pdf')
-		const pdf_uri = `${FileSystem.documentDirectory}${pdf_title}`
-
+	if (!uri) return false
+	try {
+		const pdf_uri = `${FileSystem.cacheDirectory}craft.pdf`
+		await new Promise(resolve => setTimeout(resolve, 600))
 		const converted = await convertPDF(uri, pdf_uri)
-		if (!converted) {
-			Alert.alert(t('craft'), t('craftError'))
-			return false
-		}
+		if (!converted) return false
 		Sharing.shareAsync(pdf_uri)
 		await FileSystem.deleteAsync(uri, { idempotent: true })
 		return true
+	} catch {}
+	finally {
+		FileSystem.deleteAsync(pdf_uri, { idempotent: true })
 	}
-	return false
 }
